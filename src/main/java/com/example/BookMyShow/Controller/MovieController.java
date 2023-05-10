7702e729595fc716dfedfb7e0e57f8724f75b1fa
piyush21malhotra <101010021+piyush21malhotra@users.noreply.github.com>
2023-05-11 01:01:50 +0530

    Test Commit

--- src/main/java/com/example/BookMyShow/Controller/MovieController.java
+++ src/main/java/com/example/BookMyShow/Controller/MovieController.java
@@ -17,13 +17,14 @@ public class MovieController {
     @Autowired
     MovieServiceImpl movieService;
 
-//    Post Api to add the new movie to the repository
+//    Post Api to add the new movie to the Database
     @PostMapping("/add")
     public ResponseEntity<MovieResponseDto> addMovie(@RequestBody() MovieEntryDto movieEntryDto) {
         MovieResponseDto movie = movieService.addMovie(movieEntryDto);
         return new ResponseEntity<>(movie, HttpStatus.CREATED);
     }
 
+    //    Get Api to get the movie from the Database
     @GetMapping("/get-movie/{id}")
     public ResponseEntity<MovieResponseDto> getMovieById(@PathVariable int id) {
         MovieResponseDto movie = movieService.getMovieById(id);
